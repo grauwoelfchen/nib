be7b4c67c19befa24c9e0bd9a51c91e6d0207795
Yasuhiro Яша Asaka <yasuhiro.asaka@grauwoelfchen.net>
2020-04-27 23:48:35 +0900

    Update cleaning as targeted only html files

--- src/file.rs
+++ src/file.rs
@@ -1,6 +1,18 @@
+use std::fs;
+use std::io::Error;
+
 use glob::glob;
 
 /// gets file entries.
 pub fn get_entries(path: &str) -> glob::Paths {
     glob(path).expect("failed to read glob pattern")
+}
+
+/// deletes generated results.
+pub fn rem_results(path: &str) -> Result<(), Error> {
+    let results = glob(path).expect("failed to read glob pattern");
+    for f in results.filter_map(std::result::Result::ok) {
+        fs::remove_file(f)?;
+    }
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,21 @@ use std::path::Path;
 
 use libbeta::writer::{write_entry, make_index};
 use libbeta::registry::{add_escape_fn, rem_escape_fn, load_registry};
-use libbeta::file::get_entries;
+use libbeta::file::{get_entries, rem_results};
 
 const SRC_DIR: &str = "blog";
 const DST_DIR: &str = "dst";
 
 fn main() -> std::io::Result<()> {
+    let mut ptrn;
+
     let dst_dir = Path::new(DST_DIR);
     if dst_dir.exists() {
-        fs::remove_dir_all(DST_DIR)?;
+        ptrn = dst_dir.join("*.html");
+        let dst = ptrn
+            .to_str()
+            .ok_or_else(|| Error::new(ErrorKind::Other, "unexpected dst"))?;
+        rem_results(dst)?;
     }
     fs::create_dir_all(DST_DIR)?;
 
@@ -27,7 +33,7 @@ fn main() -> std::io::Result<()> {
     if !src_dir.exists() {
         return Err(Error::new(ErrorKind::NotFound, "no src directory"));
     }
-    let ptrn = src_dir.join("*.rst");
+    ptrn = src_dir.join("*.rst");
     let src = ptrn
         .to_str()
         .ok_or_else(|| Error::new(ErrorKind::Other, "unexpected src"))?;
